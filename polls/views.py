cf314c952be9c1b1f4f04984b5049b7c29e0bc87
Joe Havelick <joe.have@gmail.com>
2013-07-26 08:56:54 -0400

    Initial Commit

---
+++ polls/views.py
@@ -0,0 +1,64 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
+from polls.models import *
+
+# def index(request):
+# 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+# 	context =  {'latest_poll_list': latest_poll_list}
+# 	return render(request, 'polls/index.html', context)
+
+# def detail(request, poll_id):
+# 	poll = get_object_or_404(Poll, pk=poll_id)
+# 	return render(request, 'polls/detail.html', {'poll': poll})
+
+# def results(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/results.html', {'poll': poll})
+
+
+class IndexView(generic.ListView):
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+
+    def get_queryset(self):
+        """
+        Exclude polls that aren't publihed yet
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/poll_results.html'
+
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
